1485997024d83acd144b7f657cf5b120b61f5bc6
supunkamburugamuva <supun06@gmail.com>
2014-03-07 16:39:39 -0500

    adding thrift generated files

--- iotcloud-core/src/main/java/cgl/iotcloud/core/SensorMaster.java
+++ iotcloud-core/src/main/java/cgl/iotcloud/core/SensorMaster.java
@@ -1,6 +1,13 @@
 package cgl.iotcloud.core;
 
+import cgl.iotcloud.core.store.InMemorySensorData;
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TNonblockingServerTransport;
+import org.apache.thrift.transport.TTransportException;
+
 import java.util.Map;
+import java.util.concurrent.Executors;
 
 public class SensorMaster {
     public static void main(String[] args) {
@@ -8,9 +15,19 @@ public class SensorMaster {
         Map conf = Utils.readConfig();
 
         // configures the sensor store
-
+        InMemorySensorData sensorStore = new InMemorySensorData();
 
         // now start the server to listen for the sites
-
+        try {
+            TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(port);
+            server = new THsHaServer(
+                    new THsHaServer.Args(serverTransport).processor(
+                            new TNodeService.Processor <NodeServiceHandler>(new NodeServiceHandler(ioTCloud))).executorService(Executors.newFixedThreadPool(10)));
+            server.serve();
+        } catch (TTransportException e) {
+            String msg = "Error starting the Thrift server";
+            log.error(msg);
+            throw new IOTException(msg);
+        }
     }
 }
