93362363a3bff6be83d36a6ac17a074ec6260cd3
Supun <supun@supun-All-Series.(none)>
2014-03-10 00:09:32 -0400

    adding to the jms transport

--- iotcloud-core/src/main/java/cgl/iotcloud/core/transport/Transport.java
+++ iotcloud-core/src/main/java/cgl/iotcloud/core/transport/Transport.java
@@ -5,6 +5,8 @@ import java.util.Map;
 public interface Transport {
     void configure(Map properties);
 
+    void registerChannel(String name, Channel channel);
+
     void start();
 
     void stop();
