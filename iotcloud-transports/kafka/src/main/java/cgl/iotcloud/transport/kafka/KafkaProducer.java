dfd2da4a1d02dc1fc277bf3b01447427b64bfcc2
supunkamburugamuva <supun06@gmail.com>
2014-03-31 14:25:57 -0400

    adding more

--- iotcloud-transports/kafka/src/main/java/cgl/iotcloud/transport/kafka/KafkaProducer.java
+++ iotcloud-transports/kafka/src/main/java/cgl/iotcloud/transport/kafka/KafkaProducer.java
@@ -1,6 +1,71 @@
 package cgl.iotcloud.transport.kafka;
 
+import cgl.iotcloud.core.transport.MessageConverter;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
 
 public class KafkaProducer {
+    private static Logger LOG = LoggerFactory.getLogger(KafkaProducer.class);
 
+    private Producer<String, String> producer;
+
+    private MessageConverter converter;
+
+    private BlockingQueue outQueue;
+
+    private String topic;
+
+    public void start() {
+        long events = Long.parseLong("10");
+        Random rnd = new Random();
+
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "broker1:9092,broker2:9092 ");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "example.producer.SimplePartitioner");
+        props.put("request.required.acks", "1");
+
+        ProducerConfig config = new ProducerConfig(props);
+
+        producer = new Producer<String, String>(config);
+    }
+
+    private class Worker implements Runnable {
+        @Override
+        public void run() {
+            boolean run = true;
+            int errorCount = 0;
+            while (run) {
+                try {
+                    try {
+                        Object input = outQueue.take();
+                        Object converted = converter.convert(input, null);
+
+                        KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, "key", (String) converted);
+                        producer.send(data);
+                    } catch (InterruptedException e) {
+                        LOG.error("Exception occurred in the worker listening for consumer changes", e);
+                    }
+                } catch (Throwable t) {
+                    errorCount++;
+                    if (errorCount <= 3) {
+                        LOG.error("Error occurred " + errorCount + " times.. trying to continue the worker");
+                    } else {
+                        LOG.error("Error occurred " + errorCount + " times.. terminating the worker");
+                        run = false;
+                    }
+                }
+            }
+            String message = "Unexpected notification type";
+            LOG.error(message);
+            throw new RuntimeException(message);
+        }
+    }
 }
