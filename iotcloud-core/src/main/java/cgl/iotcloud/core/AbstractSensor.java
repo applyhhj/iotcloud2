6701b5c814dc7d6d2de0d83764ab8c1cff3455a2
Supun <supun@supun-All-Series.(none)>
2014-04-01 16:58:01 -0400

    adding functionalities to ease the dev of sensors

--- iotcloud-core/src/main/java/cgl/iotcloud/core/AbstractSensor.java
+++ iotcloud-core/src/main/java/cgl/iotcloud/core/AbstractSensor.java
@@ -3,23 +3,48 @@ package cgl.iotcloud.core;
 import cgl.iotcloud.core.transport.Channel;
 import org.slf4j.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 
 public abstract class AbstractSensor {
-    private final Logger Log;
+    private Map<String, QueueProducer> producers = new HashMap<String, QueueProducer>();
 
-    protected AbstractSensor(Logger log) {
-        Log = log;
+    private Map<String, QueueListener> listeners = new HashMap<String, QueueListener>();
+
+    public void startChannel(Channel channel, MessageSender sender) {
+        QueueProducer producer = new QueueProducer(channel.getInQueue(), sender);
+        producers.put(channel.getName(), producer);
+
+        Thread t = new Thread(producer);
+        t.start();
     }
 
-    public void startChannel(Channel channel) {
+    public void startChannel(Channel channel, MessageReceiver receiver) {
+        QueueListener listener = new QueueListener(channel.getOutQueue(), receiver);
 
+        listeners.put(channel.getName(), listener);
+
+        Thread t = new Thread(listener);
+        t.start();
     }
 
-    private class QueueProducer implements Runnable {
+    public void stopChannel(String name) {
+        if (producers.containsKey(name)) {
+            producers.get(name).stop();
+        }
+
+        if (listeners.containsKey(name)) {
+            listeners.get(name).stop();
+        }
+    }
+
+    protected class QueueProducer implements Runnable {
         private BlockingQueue queue;
 
         private MessageSender messageSender;
+
+        private boolean run = true;
 
         private QueueProducer(BlockingQueue queue, MessageSender handler) {
             this.queue = queue;
@@ -28,36 +53,44 @@ public abstract class AbstractSensor {
 
         @Override
         public void run() {
-            while (true) {
+            while (run) {
                 messageSender.loop(queue);
             }
         }
+
+        public void stop() {
+            run = false;
+        }
     }
 
-    private class QueueListener implements Runnable {
+    protected class QueueListener implements Runnable {
         private BlockingQueue queue;
 
         private MessageReceiver messageReceiver;
 
         private Logger logger;
+        private boolean run = true;
 
-        private QueueListener(BlockingQueue queue, MessageReceiver handler, Logger logger) {
+        private QueueListener(BlockingQueue queue, MessageReceiver handler) {
             this.queue = queue;
             this.messageReceiver = handler;
-            this.logger = logger;
         }
 
         @Override
         public void run() {
-            while (true) {
+            while (run) {
                 try {
                     Object o = queue.take();
 
                     messageReceiver.onMessage(o);
                 } catch (InterruptedException e) {
-                    logger.warn("Failed to retrieve the message");
+                    throw new RuntimeException(e);
                 }
             }
+        }
+
+        public void stop() {
+            run = false;
         }
     }
 }
---
+++ iotcloud-examples/src/main/java/cgl/iotcloud/examples/chat/RabbitMQSensor.java
@@ -0,0 +1,7 @@
+package cgl.iotcloud.examples.chat;
+
+import cgl.iotcloud.core.AbstractSensor;
+
+public class RabbitMQSensor extends AbstractSensor {
+        
+}
