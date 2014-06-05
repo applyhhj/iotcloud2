79fe741a4ddf084647f0bd790697fa396c78d115
supunkamburugamuva <supun06@gmail.com>
2014-06-05 15:01:25 -0400

    improving the submitter

--- core/src/main/java/cgl/iotcloud/core/SensorSubmitter.java
+++ core/src/main/java/cgl/iotcloud/core/SensorSubmitter.java
@@ -5,10 +5,42 @@ import cgl.iotcloud.core.sensorsite.SensorDeployDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 
 public class SensorSubmitter {
     private static Logger LOG = LoggerFactory.getLogger(SensorSubmitter.class);
+
+    public static void submitSensor(Map<String, String> properties, String jarName, String className, List<String> sites) {
+        Map conf = Utils.readConfig();
+        SensorClient client = SensorClient.getConfiguredClient(conf);
+
+        SensorDeployDescriptor deployDescriptor = new SensorDeployDescriptor(jarName, className);
+        deployDescriptor.addDeploySites(sites);
+
+        for (Map.Entry<String, String> e : properties.entrySet()) {
+            deployDescriptor.addProperty(e.getKey(), e.getValue());
+        }
+
+        client.deploySensor(deployDescriptor);
+
+        client.close();
+    }
+
+    public static void submitSensor(Map conf, Map<String, String> properties, String jarName, String className, List<String> sites) {
+        SensorClient client = SensorClient.getConfiguredClient(conf);
+
+        SensorDeployDescriptor deployDescriptor = new SensorDeployDescriptor(jarName, className);
+        deployDescriptor.addDeploySites(sites);
+
+        for (Map.Entry<String, String> e : properties.entrySet()) {
+            deployDescriptor.addProperty(e.getKey(), e.getValue());
+        }
+
+        client.deploySensor(deployDescriptor);
+
+        client.close();
+    }
 
     /**
      * Submit the sensor according to the configuration given
