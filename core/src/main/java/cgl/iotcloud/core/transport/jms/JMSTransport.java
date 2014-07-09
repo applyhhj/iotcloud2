40f8822ecaa232e471d7f2aa2d236c12e747ee37
Supun <supun06@gmail.com>
2014-07-09 01:08:37 -0400

    adding the channel grouping support

--- core/src/main/java/cgl/iotcloud/core/transport/jms/JMSTransport.java
+++ core/src/main/java/cgl/iotcloud/core/transport/jms/JMSTransport.java
@@ -3,60 +3,20 @@ package cgl.iotcloud.core.transport.jms;
 import cgl.iotcloud.core.Configuration;
 import cgl.iotcloud.core.msg.MessageContext;
 import cgl.iotcloud.core.transport.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jms.*;
 import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.naming.Reference;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 
 public class JMSTransport extends AbstractTransport {
     private static Logger LOG = LoggerFactory.getLogger(JMSTransport.class);
-    /** The JMS ConnectionFactory this definition refers to */
-    private ConnectionFactory conFactory = null;
-    /** Initial context */
-    private Context context = null;
-
-    private Map<ChannelName, JMSSender> senders = new HashMap<ChannelName, JMSSender>();
-    private Map<ChannelName, JMSListener> listeners = new HashMap<ChannelName, JMSListener>();
-
-    private String siteId;
 
     @Override
-    public void configure(String siteId, Map properties) {
-        // we are configuring everything by ourselves, no need to use the super
-        try {
-            this.siteId = siteId;
-            Hashtable<String, String> params = new Hashtable<String, String>();
-            params.putAll((Map)properties.get(Configuration.TRANSPORT_PROPERTIES));
-
-            context = new InitialContext(params);
-            conFactory = lookup(context, javax.jms.ConnectionFactory.class,
-                    params.get(Configuration.IOT_SENSOR_SITE_CONFAC_JNDI_NAME));
-            LOG.debug("JMS ConnectionFactory initialized");
-        } catch (NamingException e) {
-            String msg = "Cannot acquire JNDI context, JMS Connection factory : " +
-                    properties.get(Configuration.IOT_SENSOR_SITE_CONFAC_JNDI_NAME);
-            LOG.error(msg);
-            throw new RuntimeException(msg, e);
-        } catch (Exception e) {
-            String msg = "Cannot acquire JNDI context, JMS Connection factory : " +
-                    properties.get(Configuration.IOT_SENSOR_SITE_CONFAC_JNDI_NAME);
-            LOG.error(msg);
-            throw new RuntimeException(msg, e);
-        }
-    }
-
-    @Override
-    public void configureTransport() {
-
-    }
+    public void configureTransport() {}
 
     @Override
     public Manageable registerProducer(BrokerHost host, Map channelConf, BlockingQueue<MessageContext> queue) {
@@ -66,6 +26,8 @@ public class JMSTransport extends AbstractTransport {
         if (isTopic != null && !Boolean.parseBoolean(isTopic)) {
             topic = false;
         }
+
+        ActiveMQConnectionFactory conFactory = new ActiveMQConnectionFactory(host.getUrl());
 
         JMSSender sender = new JMSSender(conFactory, siteId + "." + destination, topic, queue);
         sender.start();
@@ -80,6 +42,8 @@ public class JMSTransport extends AbstractTransport {
         if (isTopic != null && !Boolean.parseBoolean(isTopic)) {
             topic = false;
         }
+
+        ActiveMQConnectionFactory conFactory = new ActiveMQConnectionFactory(host.getUrl());
 
         JMSListener listener = new JMSListener(conFactory, siteId + "." + destination, topic, queue);
         listener.start();
